c8660ed1a09069a387b2e3c512b00c3b67273c62
Bstuparek <bjoern.stuparek@stud.hcw.ac.at>
2026-06-09 20:49:48 +0200

    Refactoring to restore proper Layering Logic

---
+++ main/Server/routes/climateRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const climateController = require('../controllers/climateController.js');
+
+router.get(":/city", climateController.getClimate);
+module.exports = router;
