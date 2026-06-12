00f4cbf083c38e6b38b12e3ad1c8d95ed7857dec
Bstuparek <bjoern.stuparek@stud.hcw.ac.at>
2026-06-12 15:08:42 +0200

    recommendationsController and weatherController now send 201 status response on success

--- main/Server/controllers/recommendationController.js
+++ main/Server/controllers/recommendationController.js
@@ -7,13 +7,19 @@ const recommendationService = require("../services/recommendationService")
                 month,
                 vacationType
             } = req.body;
-
+            if(!month || !vacationType || month == null || vacationType == null){
+                return res.status(400).json({
+                    success: false,
+                    message: "Missing vacation Details"
+                });
+            }
             const recommendations = await recommendationService.getRecommendations(month, vacationType);
 
             res.json(recommendations)
         } catch(error){
-            res.status(500).json()({
-                error: "Failed to get recommendations"
-            })
+            res.status(500).json({
+                succes: false,
+                message: "Server Error generating Recommendations"
+            });
         }
-    }
+    };
