cd5b72b1b0be235f3067378ff9f54ab39aad1e36
simon-fuer <simon.fuersatz@gmail.com>
2026-06-12 21:29:01 +0200

    bug fix

--- main/Server/server.js
+++ main/Server/server.js
@@ -1,9 +1,5 @@
 const dns = require('node:dns');
 dns.setServers(['1.1.1.1', '8.8.8.8']);
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
