45230569037c86c8e45033a03a4b22c5867fd3f1
Bstuparek <bjoern.stuparek@stud.hcw.ac.at>
2026-06-13 22:41:07 +0200

    fixed bug with dates being incorrect

--- main/Server/services/recommendationService.js
+++ main/Server/services/recommendationService.js
@@ -45,26 +45,28 @@ function generateTripDates(month, duration) {
     const currentYear = new Date().getFullYear();
 
     const monthIndexMap = {
-        Jan: 1,
-        Feb: 2,
-        Mar: 3,
-        Apr: 4,
-        May: 5,
-        Jun: 6,
-        Jul: 7,
-        Aug: 8,
-        Sep: 9,
-        Oct: 10,
-        Nov: 11,
-        Dec: 12
+        Jan: 0,
+        Feb: 1,
+        Mar: 2,
+        Apr: 3,
+        May: 4,
+        Jun: 5,
+        Jul: 6,
+        Aug: 7,
+        Sep: 8,
+        Oct: 9,
+        Nov: 10,
+        Dec: 11
     }
     const monthIndex = monthIndexMap[month];
 
     const startDate = new Date(currentYear, monthIndex, 1);
+    console.log(startDate)
 
     const endDate = new Date(startDate)
 
-    endDate.setDate(endDate.getDate() + duration);
+    endDate.setDate(endDate.getDate() + Number(duration));
+    console.log(endDate)
 
     return {
         startDate,
