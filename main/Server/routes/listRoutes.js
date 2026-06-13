95bbbe5c7eb4c59cf491022eef74c8ac88b48fa3
Bstuparek <bjoern.stuparek@stud.hcw.ac.at>
2026-06-13 16:52:20 +0200

    bugfixes

--- main/Server/routes/listRoutes.js
+++ main/Server/routes/listRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const listController = require('../controllers/listController.js');
+const auth = require("../middleware/authMiddleware");
 
 
-router.put('/packingList', auth, listController.updatePackingList);
+router.put('/packingList', auth, listController.updateLists);
 module.exports = router;
